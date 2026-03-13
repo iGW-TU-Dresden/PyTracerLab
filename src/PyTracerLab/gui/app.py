9b1d2b4a17ea32119061ff54222cfd9c69320d9a
MaxGRudolph <77970220+MaxGRudolph@users.noreply.github.com>
2026-03-13 18:12:30 +0100

    fix gui dpi scaling issues

--- src/PyTracerLab/gui/app.py
+++ src/PyTracerLab/gui/app.py
@@ -1,10 +1,19 @@
 """Application entry point for the PyTracerLab GUI."""
 
+import os
 import sys
 
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication
 
 from .main_window import MainWindow
+
+# make high dpi adjustments
+QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
+os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "1"
+os.environ["QT_SCALE_FACTOR"] = "1"
 
 
 def main():
